83d99ac0ec55c647314bc1e5f3236682955f57e8
unnamemik <unnamemik@gmail.com>
2024-02-13 03:06:33 +0300

    prod v1.0.0

--- diploma/urls.py
+++ diploma/urls.py
@@ -5,6 +5,9 @@ from diploma import views
 urlpatterns = [
     path('', views.diploma, name='diploma'),
     path('structure/', views.structure, name='structure'),
+    path('structure-mobile/', views.structure_mobile, name='structure_mobile'),
     path('techno/', views.techno, name='techno'),
+    path('techno-mobile/', views.techno_mobile, name='techno_mobile'),
     path('files/', views.files, name='files'),
+    path('files-mobile/', views.files_mobile, name='files_mobile'),
 ]
--- diploma/views.py
+++ diploma/views.py
@@ -9,8 +9,17 @@ def diploma(request):
 def structure(request):
     return render(request, "structure.html")
 
+def structure_mobile(request):
+    return render(request, "structure-mobile.html")
+
 def files(request):
     return render(request, "files.html")
 
+def files_mobile(request):
+    return render(request, "files-mobile.html")
+
 def techno(request):
     return render(request, "techno.html")
+
+def techno_mobile(request):
+    return render(request, "techno-mobile.html")
